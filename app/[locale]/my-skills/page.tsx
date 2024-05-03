0f14d790832eaacacbf469f29baf4a072dbdff3b
Aneesh Anirudhan <aneesh.anirudhan@outlook.com>
2024-05-03 16:46:59 +0100

    add my skills

--- app/[locale]/my-skills/page.tsx
+++ app/[locale]/my-skills/page.tsx
@@ -1,12 +1,27 @@
+import Navbar from '@/app/_components/Navbar';
 import SkillMatrix from '@/app/_components/SkillMatrix';
 import Skills from '@/app/_components/Skills';
-import { fetchSkills } from '@/app/helpers/fetchData';
-import React from 'react'
+import { fetchSkills, getHomePageContent } from '@/app/helpers/fetchData';
+import React from 'react';
 
-const MySkillsPage = async () => {
+type Params = {
+    params: {
+        locale: string;
+    }
+};
+
+const MySkillsPage = async ({ params: { locale } }: Params) => {
+    console.log(locale);
+    const content = await getHomePageContent(locale);
     const skills = await fetchSkills();
   return (
-    <><SkillMatrix skills={skills} /><Skills skills={skills} /></>
+    <>
+      <Navbar
+        title={content.NavSection.title}
+        cta_text={content.NavSection.cta_text}
+      />
+        <SkillMatrix skills={skills} /><Skills skills={skills} />
+    </>
   )
 }
 
