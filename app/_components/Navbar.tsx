d17ad916e6c48d2db7187e4d73bcf97d876fbd98
Aneesh Anirudhan <aneesh.anirudhan@outlook.com>
2024-05-12 12:01:56 +0100

    chane

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -87,28 +87,6 @@ const Navbar = ({ title, cta_text }: Props) => {
         </div>
       </div>
     </nav>
-    // <div className="w-full">
-    //   <nav className="container relative flex flex-wrap items-center justify-between p-8 mx-auto lg:justify-between xl:px-0">
-    //     <div className="flex flex-wrap items-center justify-between w-full lg:w-auto">
-    //       <Link href="/">
-    //         <span className="flex items-center space-x-2 text-2xl font-medium text-gray-100">
-    //           <span>{title}</span>
-    //         </span>
-    //       </Link>
-    //     </div>
-
-    //     <div className="hidden mr-3 space-x-4 lg:flex nav__item">
-    //       <Link href="/fr">Français</Link>
-    //       <Link href="/en">English</Link>
-    //       <Link
-    //         href="/contact-me"
-    //         className="px-6 py-2 text-white bg-indigo-600 rounded-md md:ml-5"
-    //       >
-    //         {cta_text}
-    //       </Link>
-    //     </div>
-    //   </nav>
-    // </div>
   );
 };
 
