37226c4b79b7f57e4cfe5edff4b014ebd8b4e07f
Aneesh Anirudhan <aneesh.anirudhan@outlook.com>
2024-05-14 17:08:34 +0100

    remover freelancer

--- app/_components/HeroHeading.tsx
+++ app/_components/HeroHeading.tsx
@@ -7,7 +7,7 @@ const HeroHeading = () => {
     <h1 className="text-5xl font-bold leading-snug tracking-tight max-lg:pt-10 md:text-6xl lg:text-7xl lg:leading-tight xl:text-8xl xl:leading-tight text-white">
       I am a <br />
       <ReactTyped
-        strings={["Developer", "Freelancer", "Consultant"]}
+        strings={["Developer", "Technologist", "Consultant"]}
         typeSpeed={50}
         loop
         backSpeed={20}
