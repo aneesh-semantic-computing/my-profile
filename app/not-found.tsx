8218b1d00a11c89b15a27c33d38c6c60ae1bdb0a
Aneesh Anirudhan <aneesh.anirudhan@outlook.com>
2024-05-21 16:36:58 +0100

    fix attribute

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const NotFound = () => {
   return (
-    <div class="flex justify-center items-center h-screen">
-        <div class="text-center">
-            <h1 class="text-4xl font-medium">404</h1>
-            <p class="text-xl font-medium m-6">Sorry, the page you&apos;re looking for can&apos;t be found.</p>
-            <a href="/" class="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded">Go Home</a>
+    <div className="flex justify-center items-center h-screen">
+        <div className="text-center">
+            <h1 className="text-4xl font-medium">404</h1>
+            <p className="text-xl font-medium m-6">Sorry, the page you&apos;re looking for can&apos;t be found.</p>
+            <a href="/" className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded">Go Home</a>
         </div>
     </div>
   )
