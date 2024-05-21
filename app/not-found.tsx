3714d2e7d4707c9bb9d6976f92a7d877513a4c5c
Aneesh Anirudhan <aneesh.anirudhan@outlook.com>
2024-05-21 16:23:11 +0100

    formatting

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,7 +5,7 @@ const NotFound = () => {
     <div class="flex justify-center items-center h-screen">
         <div class="text-center">
             <h1 class="text-4xl font-medium">404</h1>
-            <p class="text-xl font-medium m-6">Sorry, the page you're looking for can't be found.</p>
+            <p class="text-xl font-medium m-6">Sorry, the page you&apos;re looking for can&apos;t be found.</p>
             <a href="/" class="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded">Go Home</a>
         </div>
     </div>
